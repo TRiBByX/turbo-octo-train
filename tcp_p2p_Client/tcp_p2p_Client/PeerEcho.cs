54550a619ee2021ffa4c1a86d18757f286c4fa89
Christoffer Jepsen <Christoffer.ssj@gmail.com>
2016-10-28 12:30:01 +0200

    Add files via upload
    Added a client project to negate same solution probles

---
+++ tcp_p2p_Client/tcp_p2p_Client/PeerEcho.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_P2P_Client
+{
+    class PeerEcho
+    {
+        private TcpClient connectionseet;
+
+        public PeerEcho(TcpClient connectionseet)
+        {
+            this.connectionseet = connectionseet;
+        }
+
+        internal void DoIt()
+        {
+
+            Stream connectionStream = connectionseet.GetStream();
+
+            StreamReader sReader = new StreamReader(connectionStream);
+            StreamWriter sWriter = new StreamWriter(connectionStream);
+            sWriter.AutoFlush = true;
+
+            while (true)
+            {
+                string message = "";
+                sWriter.WriteLine(message);
+                Console.WriteLine($"Server: {sReader.ReadLine()}");
+            }
+        }
+    }
+}
---
+++ tcp_p2p_Client/tcp_p2p_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TCP_P2P_Client;
+
+namespace tcp_p2p_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener myServer = new TcpListener(IPAddress.Any, 9000);
+            myServer.Start();
+
+            while (true)
+            {
+                TcpClient myServerTcpconnection = myServer.AcceptTcpClient();
+                Console.WriteLine("Connection Established");
+                var pecho = new PeerEcho(myServerTcpconnection);
+                Thread myThread = new Thread(pecho.DoIt);
+                myThread.Start();
+            }
+        }
+    }
+}
